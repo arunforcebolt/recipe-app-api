02de609b801d225f67311fda96ee7211b5a27724
Arun Saharan <arunsaharan@forcebolt.com>
2021-02-11 16:30:29 -0800

    configure postgress db

--- app/core/test/test_admin.py
+++ app/core/test/test_admin.py
@@ -31,10 +31,10 @@ class AdminSiteTYest(TestCase):
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
         """Test this the create user page works"""
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)

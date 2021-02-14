fd10c2702bd196874ffef80fd978404316a1b1ee
arunforcebolt <arunsaharan2forcebolt.com>
2021-02-13 17:52:00 -0800

    Add Recipe model

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -61,3 +61,13 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+    def test_recipe_str(self):
+        """Test the recipe string respresentation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title="test title",
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -73,12 +73,11 @@ class PrivateTagsApiTests(TestCase):
             user=self.user,
             name=payload['name']
         ).exists()
-        # self.assertTrue(exists)
+        self.assertTrue(exists)
 
     def test_create_tag_invalid(self):
         """Test creating a new tag with invalid payload"""
         payload = {'name': ''}
         res = self.client.post(TAGS_URL, payload)
 
-        # self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
